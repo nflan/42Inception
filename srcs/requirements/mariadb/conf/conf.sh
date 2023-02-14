1ac4c86aee2fb3a3ed396e6462c8429450489ac7
root <root@debinception.debinception>
2023-02-14 19:02:18 +0100

    on avance sur la db mais pas clair

--- srcs/requirements/mariadb/conf/conf.sh
+++ srcs/requirements/mariadb/conf/conf.sh
@@ -1,10 +1,26 @@
 #! /bin/bash
 
-cd /tmp
-apt install wget -y
-apt install -y lsb-release
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.24-1_all.deb
-dpkg -i mysql-apt-config_0.8.24-1_all.deb
-apt update
-apt install mysql-server -y
+cat << OEF > config.sql
+ALTER USER 'root'@'inception' IDENTIFIED BY 'incept42'
+CREATE USER 'nflan42'@'inception' IDENTIFIED BY 'nflan42'
+CREATE DATABASE wordpress
+GRANT ALL PRIVILEGES ON wordpress * . * TO 'root'@'inception' IDENTIFIED BY 'incept42'
+FLUSH PRIVILEGES
+OEF
+
+service mysql start
+usleep 5
+mysql -uroot --password="" < config.sql
+/q
+mysql -uroot --password="incept42"
+sleep infinity
 exec mysqld
+#se log a mysql avec login root et --password="" < config.sql
+# faire un heredoc pour creer le fichier config.sql
+# Alter User
+# Create User
+# database wp
+# droits sur la database
+#flush
+#eof
+
