e1e132376ef7f0ad54ca1b40e2143168703cdf6b
root <root@debinception.debinception>
2023-02-16 19:40:34 +0100

    Starting wordpress and get dependances

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ NAME= inception
 all:
 	test -d /home/nflan/data/wordpress || mkdir -p /home/nflan/data/wordpress
 	test -d /home/nflan/data/mariadb || mkdir -p /home/nflan/data/mariadb
+<<<<<<< HEAD
 	docker compose -f srcs/docker-compose.yml up --build --remove-orphans
 
 bonus:
@@ -13,6 +14,12 @@ bonus:
 
 stop:
 	docker compose -f srcs/docker-compose.yml stop && docker compose -f srcs/docker-compose_bonus.yml stop
+=======
+	cd srcs && docker compose up --build
+
+stop:
+	cd srcs && docker compose stop
+>>>>>>> a681295 (Starting wordpress and get dependances)
 
 fclean: stop
 	sudo rm -rf /home/nflan/data/*
--- srcs/requirements/wordpress/conf/conf.sh
+++ srcs/requirements/wordpress/conf/conf.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+<<<<<<< HEAD
 while ! mariadb -hmariadb -u${DATABASE_USER} -p${DATABASE_UPASS} "${DATABASE_WP}" &>/dev/null
 do
     sleep 1
@@ -35,3 +36,19 @@ fi
 
 echo "Starting php-fpm for wordpress"
 exec /usr/sbin/php-fpm7.4 -F
+=======
+#apt update
+#chown www-data:www-data /var/www/wordpress -R
+#chmod -R 777 /var/www/wordpress
+mkdir -p /var/www/html/nflan.42.fr
+pushd /var/www/html/nflan.42.fr
+wget https://wordpress.org/latest.tar.gz
+tar -xvzf latest.tar.gz
+chown -R wordpress: /var/www/html/nflan.42.fr/
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+wp core download --allow-root
+wp config create --dbname=wordpress --dbuser=nflan42 --dbpass=nflan42 --dbhost=mariadb --allow-root
+sleep infinity
+>>>>>>> a681295 (Starting wordpress and get dependances)
