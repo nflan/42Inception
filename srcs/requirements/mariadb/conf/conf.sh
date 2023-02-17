cf4d115cf6fc952082c7c25c807f649e0539a181
root <root@debinception.debinception>
2023-02-17 16:12:44 +0100

    bah j'ai les conditions, j'arrive pas a faire pop wp sur 443

--- srcs/requirements/mariadb/conf/conf.sh
+++ srcs/requirements/mariadb/conf/conf.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-if test -d /home/nflan/data/mariadb/wordpress
+if [ ! -d /var/lib/mysql/wordpress ]
 then
-cat << OEF > /tmp/config.sql
+	cat << BLOCK > /tmp/config.sql
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'incept42';
 CREATE USER 'nflan42'@'%' IDENTIFIED BY 'nflan42';
 CREATE DATABASE wordpress;
 GRANT ALL PRIVILEGES ON wordpress . * TO 'nflan42'@'%' IDENTIFIED BY 'nflan42';
-FLUSH PRIVILEGES;
-OEF
+FLUSH PRIVILEGES
+BLOCK
 
 service mysql start
 until mysql -uroot --password="" &>/dev/null
@@ -17,18 +17,8 @@ do
 	sleep 1
 done
 sleep 1
-echo "Param"
 mysql -uroot --password="" < /tmp/config.sql
 mysqladmin --user=root --password=incept42 shutdown
 sleep 1
 fi
 exec mysqld --bind-address=0.0.0.0
-#se log a mysql avec login root et --password="" < config.sql
-# faire un heredoc pour creer le fichier config.sql
-# Alter User
-# Create User
-# database wp
-# droits sur la database
-#flush
-#eof
-
--- srcs/requirements/wordpress/conf/conf.sh
+++ srcs/requirements/wordpress/conf/conf.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
 
 #apt update
-#chown www-data:www-data /var/www/wordpress -R
-#chmod -R 777 /var/www/wordpress
 mkdir -p /var/www/html/nflan.42.fr
 pushd /var/www/html/nflan.42.fr
-wget https://wordpress.org/latest.tar.gz
-tar -xvzf latest.tar.gz
-chown -R wordpress: /var/www/html/nflan.42.fr/
+#wget https://wordpress.org/latest.tar.gz
+#tar -xvzf latest.tar.gz
+#chown -R www-data:www-data /var/www/html/nflan.42.fr/
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-wp core download --allow-root
-wp config create --dbname=wordpress --dbuser=nflan42 --dbpass=nflan42 --dbhost=mariadb --allow-root
+if [ ! -f wp-config.php ]
+then
+	wp core download --allow-root
+	wp config create --dbname=wordpress --dbuser=nflan42 --dbpass=nflan42 --dbhost=mariadb --allow-root
+	wp core install --url=nflan.42.fr --title=Inception --admin_user=supervisor --prompt=admin_password < /tmp/adm.txt --admin_email=ok@ok.com --allow-root
+fi
 sleep infinity
