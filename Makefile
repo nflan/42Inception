e76275ce33b2096bf6906306cf5070da54befe97
Nicolas FLAN <nflan@e3r12p16.42.fr>
2024-01-04 18:02:53 +0100

    Push 125/125

--- Makefile
+++ Makefile
@@ -1,9 +1,6 @@
-NAME= inception
-
 all:
 	test -d /home/nflan/data/wordpress || mkdir -p /home/nflan/data/wordpress
 	test -d /home/nflan/data/mariadb || mkdir -p /home/nflan/data/mariadb
-<<<<<<< HEAD
 	docker compose -f srcs/docker-compose.yml up --build --remove-orphans
 
 bonus:
@@ -14,22 +11,8 @@ bonus:
 
 stop:
 	docker compose -f srcs/docker-compose.yml stop && docker compose -f srcs/docker-compose_bonus.yml stop
-=======
-	cd srcs && docker compose up --build
 
-stop:
-	cd srcs && docker compose stop
->>>>>>> a681295 (Starting wordpress and get dependances)
-
-<<<<<<< HEAD
 fclean: stop
 	sudo rm -rf /home/nflan/data/*
-=======
-fclean:
-	make stop
-	sudo rm -rf /home/nflan/data/* y
-
-.PHONY: stop
->>>>>>> acda936 (Struggling a bit)
 
 .PHONY: stop bonus fclean
--- srcs/requirements/mariadb/conf/conf.sh
+++ srcs/requirements/mariadb/conf/conf.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-<<<<<<< HEAD
 
 test -d '/var/run/mysqld/' || mkdir '/var/run/mysqld' && chmod 0755 '/var/run/mysqld' && touch '/var/run/mysqld/mysqld.sock' && chmod 0755 '/var/run/mysqld/mysqld.sock' && chown mysql:mysql '/var/run/mysqld/' -R
 
@@ -9,43 +8,10 @@ ALTER USER '${DATABASE_ADM}'@'localhost' IDENTIFIED BY '${DATABASE_PASSWORD}';
 CREATE USER '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_UPASS}';
 CREATE DATABASE ${DATABASE_WP};
 GRANT ALL PRIVILEGES ON ${DATABASE_WP} . * TO '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_UPASS}';
-=======
-if test -d /home/nflan/data/mariadb/wordpress
-then
-cat << OEF > /tmp/config.sql
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'incept42';
-CREATE USER 'nflan42'@'%' IDENTIFIED BY 'nflan42';
-CREATE DATABASE wordpress;
-GRANT ALL PRIVILEGES ON wordpress . * TO 'nflan42'@'%' IDENTIFIED BY 'nflan42';
->>>>>>> a681295 (Starting wordpress and get dependances)
 FLUSH PRIVILEGES;
 BLOCK
 
-<<<<<<< HEAD
 service mysql start &> /dev/null
-=======
-service mysql start
-until mysql -uroot --password="" &>/dev/null
-do
-	echo "Waiting for service"
-	sleep 1
-done
-sleep 1
-echo "Param"
-mysql -uroot --password="" < /tmp/config.sql
-mysqladmin --user=root --password=incept42 shutdown
-sleep 1
-fi
-exec mysqld --bind-address=0.0.0.0
-#se log a mysql avec login root et --password="" < config.sql
-# faire un heredoc pour creer le fichier config.sql
-# Alter User
-# Create User
-# database wp
-# droits sur la database
-#flush
-#eof
->>>>>>> a681295 (Starting wordpress and get dependances)
 
 sleep 1
 
